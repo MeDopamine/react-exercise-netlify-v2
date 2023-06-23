4e41a538fa5e0cced546d38b028e60f49ebed99a
MeDopamine <ramdhan.wahyusahputra.tik20@mhsw.pnj.ac.id>
2023-06-23 13:31:19 +0700

    new commit exercise

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://64952759beaa9239918872d7--exercise-netlify-wahyu.netlify.app/public/" // TODO: replace this
+const studentName = "Ramadhan Wahyu Sahputra" // TODO: replace this
+const studentId = "FE4799827" // TODO: replace this
+
+export { url, studentName, studentId }
